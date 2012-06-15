e0afaa61ba1da54b2b9e5d72beccd323db1cf241
Yves Lempereur <yves@lempereur.name>
2012-06-14 23:40:33 -0700

    Improved date handling

--- eqlog.py
+++ eqlog.py
@@ -3,27 +3,31 @@
 
 import datetime
 import argparse
+import dateutil.parser
 import os
 import re
 
-today = datetime.date.today().strftime("%b %e")
-
+iso = "%Y-%m-%d"
+today = datetime.date.today().strftime(iso)
 parser = argparse.ArgumentParser(description="Process the log.")
-parser.add_argument('-d', '--date', default=today, help="the date (eg: May 31)")
+parser.add_argument('-d', '--date', default=today, help="the date")
 parser.add_argument("toon", nargs="?", default="Karpan", help="the toon name")
 args = parser.parse_args()
+date = dateutil.parser.parse(args.date).date()
+path = os.path.expanduser("~/Library/Application Support/EverQuest/PlayerLogs/eqlog_" + args.toon + "_52.txt")
 
 db = {}
 i = 0
-path = os.path.expanduser("~/Library/Application Support/EverQuest/PlayerLogs/eqlog_" + args.toon + "_52.txt")
 with open(path, 'r') as f:
-    dt = r"\[(Mon|Tue|Wed|Thu|Fri|Sat|Sun) ((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [ 1-3]\d) [0-2]\d:[0-5]\d:[0-5]\d [1-2]\d{3}\] "
+    dt = r"\[(Mon|Tue|Wed|Thu|Fri|Sat|Sun) ((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [ 1-3]\d) [0-2]\d:[0-5]\d:[0-5]\d ([1-2]\d{3})\] "
     nm = r"( AFK )?\[(ANONYMOUS|[1-6]?\d [ A-Za-z]+)\] ([A-Z][a-z]*) (\([ A-Za-z]+\))?( <[ A-Za-z]+>)?( LFG)?"
     ct = r"There are \d+ players in [ A-Za-z,-]+\."
     fl = "T123456789abcdefghijklmnopqrstuvwxyz"
+    yr = date.strftime("%Y")
+    md = date.strftime("%b %e")
     for line in f:
         m = re.match(dt, line)
-        if m is None or m.group(2) != args.date: continue
+        if m is None or m.group(4) != yr or m.group(2) != md: continue
         line = line[27:]
         m = re.match(nm, line)
         if m is not None:
@@ -38,6 +42,8 @@ with open(path, 'r') as f:
             i = i + 1
             continue
         #print line,
+print date.strftime(iso)
+print
 names = sorted(db.keys())
 for name in names:
     print name, db[name]
